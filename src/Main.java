dd7ebcea9a4c2e2a06c37e39af4b0bae307ce66c
Arty <kirya26763@mail.ru>
2025-01-10 22:15:46 +0700

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Registration.entrance("java_skypro_go","D_1hWiKjjP_9","D_1hWiKjjP_9");
+        Registration.entrance("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
     }
 }
